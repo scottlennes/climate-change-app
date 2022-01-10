c3652eb80d4ead484dc9437dc272e24576c07793
Scott Lennes <scott.lennes@oliverwyman.com>
2022-01-09 20:47:47 -0600

    additional inputs

--- app/server.R
+++ app/server.R
@@ -2,11 +2,12 @@
 
 server <- function(input, output) {
   
-  output$TempOverTime = renderPlot({
+  output$CountryTempOverTime = renderPlot({
     
-    idt = data %>%
+    idt = temp_by_country %>%
       filter(Country %in% input$country,
-             month(Date) == 11)
+             month(Date, label = TRUE, abbr = FALSE) %in% input$month,
+             year(Date) >= input$years[1], year(Date) <= input$years[2])
 
     idt$x = idt$Date
     
@@ -15,10 +16,12 @@ server <- function(input, output) {
     } else {
       idt$y = idt$TempCelsius
     }
+
       
     ggplot(idt, aes(x = x, y = y, color = Country)) +
       geom_point(na.rm = TRUE) +
-      geom_smooth(method = "lm", formula = y ~ x, se = FALSE, na.rm = TRUE)
+      geom_smooth(method = "lm", formula = y ~ x, se = FALSE, na.rm = TRUE) +
+      labs(x = "Year", y = "Temperature")
     
   })
   
--- app/ui.R
+++ app/ui.R
@@ -8,7 +8,7 @@ intro_panel <- tabPanel(
   
   img(src = "intro.jpg"),
   p(),
-  p("This is a dashboard about climate change, which uses a Kaggle data set"),
+  p("This is a dashboard about climate change that I'm using to practice R Shiny."),
   p(a(href = "https://www.kaggle.com/berkeleyearth/climate-change-earth-surface-temperature-data/version/2", "Link to Kaggle data"))
 )
 
@@ -22,16 +22,28 @@ second_panel <- tabPanel(
 
     sidebarPanel(
       selectInput("country", "Country:", 
-                  unique(data$Country) %>% str_sort(),
+                  unique(temp_by_country$Country) %>% str_sort(),
                   selected = "United States",
                   multiple = TRUE),
+      
+      selectInput("month", "Month:", 
+                  month.name,
+                  selected = "January",
+                  multiple = FALSE),
+      
+      sliderInput("years", "Years to Include:",
+                  min = year(min(temp_by_country$Date)),
+                  max = year(max(temp_by_country$Date)),
+                  value = c(1950, 2000),
+                  ticks = FALSE,
+                  sep = ""),
       
       radioButtons("unit", "Unit of Measure:",
                    c("Celsius", "Fahrenheit"))
     ),
     
     mainPanel(
-      plotOutput('TempOverTime')
+      plotOutput('CountryTempOverTime')
     )
   )
   
